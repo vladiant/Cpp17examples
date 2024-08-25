3cfdac226958e1f6c2803aa8fd16d3392a5c6a26
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-08-25 10:28:16 +0300

    Sample fixes

--- filesystem/create_files/main.cpp
+++ filesystem/create_files/main.cpp
@@ -1,3 +1,14 @@
+//********************************************************
+// The following code example is taken from the book
+//  C++17 - The Complete Guide
+//  by Nicolai M. Josuttis (www.josuttis.com)
+//  http://www.cppstd17.com
+//
+// The code is licensed under a
+//  Creative Commons Attribution 4.0 International License
+//  http://creativecommons.org/licenses/by/4.0/
+//********************************************************
+
 #include <cstdlib>  // for std::exit() and EXIT_FAILURE
 #include <filesystem>
 #include <fstream>
