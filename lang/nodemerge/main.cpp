e9e037440d4d56a01d1a459821b63320b2a36089
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-09-04 18:39:23 +0300

    Add node merge example

---
+++ lang/nodemerge/main.cpp
@@ -0,0 +1,41 @@
+//********************************************************
+// The following code example is taken from the book
+//  C++17 - The Complete Guide
+//  by Nicolai M. Josuttis (www.josuttis.com)
+//  http://www.cppstd17.com
+//
+// The code is licensed under a
+//  Creative Commons Attribution 4.0 International License
+//  http://creativecommons.org/licenses/by/4.0/
+//********************************************************
+
+#include <iostream>
+#include <map>
+#include <string>
+
+template <typename T1, typename T2>
+void print(const T1& coll1, const T2& coll2) {
+  std::cout << "values:\n";
+  for (const auto& [key, value] : coll1) {
+    std::cout << "  [" << key << ":" << value << "]";
+  }
+  std::cout << '\n';
+  for (const auto& [key, value] : coll2) {
+    std::cout << "  [" << key << ":" << value << "]";
+  }
+  std::cout << '\n';
+}
+
+int main() {
+  std::multimap<double, std::string> src{
+      {1.1, "one"}, {2.2, "two"}, {3.3, "three"}};
+  std::map<double, std::string> dst{{3.3, "old data"}};
+
+  print(src, dst);
+
+  // merge all elements from src into dst:
+  dst.merge(src);
+
+  print(src, dst);
+  return 0;
+}
