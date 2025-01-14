8c5031ed25114899b00e65e8410afc17861b1703
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-01-14 08:31:56 +0200

    Add inheritance example

---
+++ poly/inherit/geoobj.hpp
@@ -0,0 +1,40 @@
+//********************************************************
+// The following code example is taken from the book
+//  C++17 - The Complete Guide
+//  by Nicolai M. Josuttis (www.josuttis.com)
+//  http://www.cppstd17.com
+//
+// The code is licensed under a
+//  Creative Commons Attribution 4.0 International License
+//  http://creativecommons.org/licenses/by/4.0/
+//********************************************************
+
+#pragma once
+
+#include <string>
+
+#include "coord.hpp"
+
+// abstract base class
+class GeoObj {
+ protected:
+  std::string name;  // name of the geometric object
+
+  GeoObj(const GeoObj&) = default;  // disable slicing
+  GeoObj(GeoObj&&) = default;       // disable slicing
+
+ public:
+  GeoObj(std::string n = {}) : name{n} {}
+
+  virtual std::string getName() && final { return std::move(name); }
+  virtual const std::string& getName() const& final { return name; }
+
+  virtual void move(Coord) = 0;
+  virtual void draw() const = 0;
+
+  virtual ~GeoObj() = default;
+  GeoObj& operator=(const GeoObj&) = delete;  // disable slicing
+  GeoObj& operator=(GeoObj&&) = delete;       // disable slicing
+
+  [[nodiscard]] virtual GeoObj* clone() const = 0;
+};
---
+++ poly/inherit/polygon.hpp
@@ -0,0 +1,54 @@
+//********************************************************
+// The following code example is taken from the book
+//  C++17 - The Complete Guide
+//  by Nicolai M. Josuttis (www.josuttis.com)
+//  http://www.cppstd17.com
+//
+// The code is licensed under a
+//  Creative Commons Attribution 4.0 International License
+//  http://creativecommons.org/licenses/by/4.0/
+//********************************************************
+
+#pragma once
+
+#include <array>
+#include <cassert>
+#include <iostream>
+
+#include "geoobj.hpp"
+
+class Polygon : public GeoObj {
+ private:
+  std::array<Coord, 100> points;
+  int size;
+
+ public:
+  // constructor for name and list of Coord's
+  Polygon(std::string n, std::initializer_list<Coord> pl)
+      : GeoObj{std::move(n)}, size{static_cast<int>(pl.size())} {
+    assert(pl.size() <= points.size());
+    int idx{0};
+    for (const auto& p : pl) {
+      points[idx++] = p;
+    }
+  }
+
+  virtual void move(Coord c) override {
+    for (int i = 0; i < size; ++i) {
+      points[i] += c;
+    }
+  }
+  virtual void draw() const override {
+    std::cout << "polygon '" << name << "' over";
+    for (int i = 0; i < size; ++i) {
+      std::cout << " " << points[i];
+    }
+    std::cout << "\n";
+  }
+
+  virtual int numPoints() const final { return size; }
+
+  [[nodiscard]] virtual Polygon* clone() const override {
+    return new Polygon{*this};
+  }
+};
